5b97178d92a8f43387d59f5f5ce2d01d72586553
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-04 23:32:00 +0000

    fixed Mistake

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // https://youtu.be/0QUgvfuKvWU?t=1103
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
-
         }
     }
 }
--- HelloWorld/HelloWorld/Properties/AssemblyInfo.cs
+++ HelloWorld/HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("HelloWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("063cd144-a515-497d-b34f-4c4a87fea4e7")]
+[assembly: Guid("bcb9355c-0d8b-441a-a2c3-a35749dfaa86")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
