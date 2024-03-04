427f2e67010fd8a847ca2a34e900d1f956c0a7dd
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-04 00:58:48 +0000

    Continue on Module-03

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/0QUgvfuKvWU?t=1103
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
