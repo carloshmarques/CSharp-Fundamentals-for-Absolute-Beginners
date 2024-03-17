996a884624cea51ac8eb1bd5a8d672f5073996e0
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-17 15:35:00 +0000

    add Dates and times module

---
+++ DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesAndTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://youtu.be/0QUgvfuKvWU?t=14118
+            
+            // Crate a DateTime object
+
+            DateTime myValue = DateTime.Now;
+            /*
+            Console.WriteLine(myValue.ToString());
+            Console.WriteLine(myValue.ToShortDateString());
+            Console.WriteLine(myValue.ToShortTimeString());
+            Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToLongTimeString());
+            */
+
+            // Add and Subtract from a DateTime object
+            /*
+            Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            Console.WriteLine(myValue.AddHours(-3).ToLongTimeString());
+            Console.WriteLine(myValue.Month);
+            */
+
+            // Create a DateTime object with a specific date
+            // DateTime myBirthday = new DateTime(1969, 12, 7);
+            // Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("12/7/1969");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+            Console.ReadLine();
+        }
+    }
+}
