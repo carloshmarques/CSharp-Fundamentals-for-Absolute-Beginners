54c3830cd4b2f065d47cecd4fc5914ba94130a87
carloshmarques <carlos.manuel.horta.marques@hotmail.com>
2024-03-20 21:21:17 +0000

    add handling exceptions module

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  https://youtu.be/0QUgvfuKvWU?t=25178
+
+            try
+            {
+                string content = File.ReadAllText(@"C:\Example\exampl.txt");               
+                Console.WriteLine(content);
+                        
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is correct");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the directory exists");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Code to finalize
+                // Setting objects to null
+                // Closing database connections
+                Console.WriteLine("Closing application now...");
+            }
+                     
+            Console.ReadLine();          
+        }
+    }
+}
